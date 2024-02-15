0e5bb8ff6d090b787ea5d324ae26dfefd211c862
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-02-15 17:15:44 +0000

    added like one line in index lol

--- Unit1-01/index.ts
+++ Unit1-01/index.ts
@@ -3,3 +3,11 @@
  *   and deal with numbers
  *
  *   By:	Flynn Rundquist
+**/
+
+import { createPrompt } from 'bun-promptx'
+
+console.log("Choose the length of logs you would like to move with a truck.")
+
+const width = createPrompt('Enter width in meters -> 0.25, 0.5, 1:')
+
