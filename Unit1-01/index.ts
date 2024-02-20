03b3fdf838724bf074a2d0357b2c9bb5f9ffab43
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-02-20 15:05:51 +0000

    tried stuff. not working

--- Unit1-01/index.ts
+++ Unit1-01/index.ts
@@ -7,7 +7,16 @@
 
 import { createPrompt } from 'bun-promptx'
 
-console.log("Choose the length of logs you would like to move with a truck.")
+console.log('\nChoose the length of logs you would like to move with a truck.')
 
-const width = createPrompt('Enter width in meters -> 0.25, 0.5, 1:')
+const lengthOfLog = createPrompt('Enter length in meters -> 0.25, 0.5, 1:')
+let lengthOfLogFloat = parseFloat(lengthOfLog.value)
+const capacity = 1100
+const weight = 20
+let maxLength = capacity / weight
+let numberOfLogs = maxLength / lengthOfLog
+
+console.log('The truck can carry ${numberOfLogs} logs that are ${lengthOfLog} meters long.')
+console.log('\nDone.')
+
 
