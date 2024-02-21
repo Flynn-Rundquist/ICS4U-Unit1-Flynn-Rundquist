f376a9691c77667e2194742431d78c91525f497f
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-02-21 22:39:43 +0000

    finally got unit 1-01 working

--- Unit1-01/index.ts
+++ Unit1-01/index.ts
@@ -3,20 +3,23 @@
  *   and deal with numbers
  *
  *   By:	Flynn Rundquist
-**/
+ *   Version: 1.0
+ *   Since: 2024/02/14
+*/
 
 import { createPrompt } from 'bun-promptx'
 
 console.log('\nChoose the length of logs you would like to move with a truck.')
 
-const lengthOfLog = createPrompt('Enter length in meters -> 0.25, 0.5, 1:')
-let lengthOfLogFloat = parseFloat(lengthOfLog.value)
+const lengthOfLogInput = createPrompt('Enter length in meters -> 0.25, 0.5, 1.0: ')
+const lengthOfLogFloat = parseFloat(lengthOfLogInput.value)
+
 const capacity = 1100
 const weight = 20
-let maxLength = capacity / weight
-let numberOfLogs = maxLength / lengthOfLog
 
-console.log('The truck can carry ${numberOfLogs} logs that are ${lengthOfLog} meters long.')
+const maxLength = capacity / weight
+const numberOfLogs = maxLength / lengthOfLogFloat
+
+console.log(`The truck can carry ${numberOfLogs} logs that are ${lengthOfLogFloat} meters long.`)
 console.log('\nDone.')
-
 
