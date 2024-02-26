c29c60d136c2ef4e807ea95e70b43769c547076c
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-02-26 20:19:36 +0000

    not working

--- Unit1-04/dice.ts
+++ Unit1-04/dice.ts
@@ -9,7 +9,10 @@
 
 import { createSelection } from 'bun-promtpx'
 
-const randomNumber = randomInt(0, 5)
+let counter = 0
+const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+while  (1) {
 
 const userGuess = createSelection([
         { text: '1' },
@@ -19,8 +22,16 @@ const userGuess = createSelection([
         { text: '5' },
         { text: '6'}
 ], {
-        headerText: 'Guess a number between 1 to 6: "
+        headerText: "Guess a number between 1 to 6: "
         perPage: 6
 })
 
+if (userGuess > randomNumber) {
+        console.log("Too high, try again."\n)
+} else if (userGuess < randomNumber) {
+        console.lof("Too low, try again."\n)
+} else {
+        console.log("Correct.")
+}}
 
+console.log("\nDone.")
