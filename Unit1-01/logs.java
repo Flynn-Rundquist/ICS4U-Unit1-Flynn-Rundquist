d544d5331f75bc139f506342a2bdab22f29cc5d8
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-02-29 23:25:04 -0500

    Update logs.java

--- Unit1-01/logs.java
+++ Unit1-01/logs.java
@@ -7,3 +7,25 @@
  * @since 2024-02-29
  */
 
+import java.util.Scanner;
+
+public class LogTruck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("\nChoose the length of logs you would like to move with a truck.");
+        System.out.print("Enter length in meters -> 0.25, 0.5, 1.0: ");
+        double lengthOfLogFloat = scanner.nextDouble();
+        
+        final int capacity = 1100;
+        final int weight = 20;
+        
+        double maxLength = (double) capacity / weight;
+        double numberOfLogs = maxLength / lengthOfLogFloat;
+        
+        System.out.printf("The truck can carry %.2f logs that are %.2f meters long.\n", numberOfLogs, lengthOfLogFloat);
+        System.out.println("\nDone.");
+        
+        scanner.close();
+    }
+}
