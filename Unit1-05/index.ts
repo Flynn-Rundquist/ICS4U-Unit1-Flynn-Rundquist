960582d105c82d42f6ef2d8d6c71b4bb178a9721
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-03-01 01:26:37 +0000

    1-05 is kind of working, but puts 1728 as the answer for everything

--- Unit1-05/index.ts
+++ Unit1-05/index.ts
@@ -9,18 +9,24 @@
 
 import { createPrompt } from 'bun-promptx'
 
+// input
 console.log("This program finds out the length of a wooden board that is 1 board foot in dimension.")
-
-const boardWidth = createPrompt("Entre width of board (inches): ")
-const boardHeight = createPrompt("Enter height of board (inches): ")
+const boardWidthString = createPrompt("Entre width of board (inches): ")
+const boardHeightString = createPrompt("Enter height of board (inches): ")
 const boardFoot = 144
+
+// error checking
+const boardWidth = parseFloat(boardWidthString.value || "-1")
+const boardHeight = parseFloat(boardHeightString.value || "-1")
 
 if (isNaN(boardWidth) == true || isNaN(boardHeight) == true) {
   console.log("Invalid input.")
 } else if (boardWidth < 0 || boardHeight < 0) {
   console.log("Invalid input.") 
 } else {
+  // process
   let boardLength = (12 * boardFoot) / (boardHeight * boardWidth)
+  // output
   console.log(`The wood should be ${boardLength} inch(es) long.`)
 }
 
