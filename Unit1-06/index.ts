df117b06eafc1a7033718063e67539af1b6f5aee
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-03-04 15:05:00 +0000

    median and mean written

--- Unit1-06/index.ts
+++ Unit1-06/index.ts
@@ -6,3 +6,39 @@
  * Since: 2024/03/04
  */
 
+// get arguments
+//
+import { readFileSync } from 'fs'
+//
+//print proccess.argv
+process.argv.forEach(function (val, index, array) {
+})
+
+
+const file = readFileSync(process.argv[2], 'utf8')
+
+const newArray = file.split(/\r?\n/)
+// pop last element, since its empty
+newArray.pop()
+console.log(newArray)
+
+function calculateMean(arr: number[]): number {
+        let sum = 0
+        for (const arrayValues of arr) {
+                sum += num
+        }
+        return sum / arr.length
+}
+
+function calculateMedian(arr: number[]): number {
+        const sortedArry = arr.slice().sort((a.b) => a - b)
+        const arrayLength = sortedArray.length
+        if (arrayLength % 2 === 0) {
+                return (sortedArray[length / 2 - 1] + sortedArray[length / 2]) / 2
+        } else {
+                return sortedArr[Math.floor(length / 2)]
+        }
+}
+
+function calculateMode(arr: number[]): number {
+        const frequencyMap: { [key: number]: number } = {}
