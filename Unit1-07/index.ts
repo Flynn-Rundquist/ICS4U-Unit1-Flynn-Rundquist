20b2269b550a1caae94fea5da497e569b871eac9
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-03-18 18:35:27 -0400

    not working

--- Unit1-07/index.ts
+++ Unit1-07/index.ts
@@ -1,6 +1,7 @@
 /**
  * This program reads a text file of students, and randomly generate marks
- * with a mean of 75 and a standard deviation of 10.
+ * with a mean of 75 and a standard deviation of 10, and passes them 
+ * back as a 2D array of strings in a file called "marks.csv".
  *
  * By: Flynn Rundquist
  * Version: 1.0
@@ -10,8 +11,17 @@
 import { readFileSynce } from 'fs'
 
 // Read files and split contents
-const studentList = readFileSync(process.argv[2], 'utf8')
-const newArray = fileContents.trim().split(/\r?\n/)
+const contents = readFileSync('./student-names.txt')
+  .toString()
+  .split('\n')
 
-console.log(newArray)
+// Generate marks
+const marks = contents
+  .map((student) => {
+    const mark = Math.round(Math.random() * 100)
+    return `${student},${mark}`
+  })
+  .join('\n')
 
+// Write marks to file
+writeFileSynce('./marks.csv', marks)
