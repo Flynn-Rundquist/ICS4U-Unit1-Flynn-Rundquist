d3b92b641763a8e2f62e0fb7fc1f778e816c0d1b
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-03-18 17:54:10 -0400

    Update Energy.java

--- Unit1-02/Energy.java
+++ Unit1-02/Energy.java
@@ -10,46 +10,30 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
-* Energy calculation program.
-*/
-
 final class Energy {
-
-    /**
-     * Constants.
-     */
-
     public static final int SPEED_OF_LIGHT = 299792458;
 
-    /**
-    * Prevent instantiation.
-    * Throw an exception IllegalStateException
-    * if this is ever called.
-    *
-    * @throws IllegalStateException if this is ever called
-    */
     private Energy() {
         throw new IllegalStateException("Cannot be instantiated");
     }
-
-    /**
-    * The starting main() function.
-    *
-    * @param args No args will be used
-    */
 
     public static void main(final String[] args) {
         try {
             final Scanner input = new Scanner(System.in);
             System.out.print("Enter the mass of the object in kilograms: ");
             final double mass = input.nextDouble();
+            
+            if (mass < 0) {
+                System.out.println("Invalid input! Mass cannot be negative.");
+                System.exit(0); // Exit the program
+            }
+            
             final double energy = mass * Math.pow(SPEED_OF_LIGHT, 2);
             System.out.println(mass + "kg of mass would produce "
                             + String.format("%.3e", energy) + " J of energy.");
             input.close();
         } catch (InputMismatchException ex) {
-            System.out.println("Invalid Input!");
+            System.out.println("Invalid input! Please enter a valid number.");
         }
 
         System.out.println("\nDone.");
